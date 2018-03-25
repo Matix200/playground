57cedd2e87e7bf7d3defd4eab392762b4d256b94
Unknown <konrad.rycerz@cannabishouse.eu>
2018-03-25 15:20:58 +0200

    add i18n (default pl_PL) & integration_medwise_template_part1

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module CannaMedica
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # Set default locales to pl_PL
+    config.i18n.default_locale = :pl
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
